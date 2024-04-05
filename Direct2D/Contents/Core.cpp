7f2121634d71c4d5cfb61404042abed5ee8eb83f
ChoiYeongHwan <cyh5343@naver.com>
2024-04-05 12:11:36 +0900

    DX 화면에 이미지 출력 업데이트

--- Direct2D/Contents/Core.cpp
+++ Direct2D/Contents/Core.cpp
@@ -12,22 +12,45 @@ UCore::~UCore()
 
 void UCore::Initialize()
 {
-	GEngine->CreateLevel<APlayGameMode>("PlayLevel");
-	GEngine->ChangeLevel("PlayLevel");
+
+	{
+		// 파일의 헤더
+		UEngineDirectory Dir;
+		Dir.MoveToSearchChild("ContentsResources");
+		Dir.Move("Miscellaneous");
+		std::vector<UEngineFile> Files = Dir.GetAllFile({ ".png" }, true);
+		for (UEngineFile& File : Files)
+		{
+			//File.Open(EIOOpenMode::Read, EIODataType::Binary);
+
+			//char Arr[100];
+			//File.Read(Arr, 100);
+
+			UEngineTexture::Load(File.GetFullPath());
+		}
+		// UEngineSound::SoundPlay("anipang_ingame_wav.wav");
+	}
+
 
 	//{
 	//	UEngineDirectory Dir;
 	//	Dir.MoveToSearchChild("ContentsResources");
 	//	Dir.Move("Sound");
-
-	//	std::list<UEngineFile> Files = Dir.AllFile({ ".wav" });
+	//	std::vector<UEngineFile> Files = Dir.GetAllFile({ ".wav" });
 	//	for (UEngineFile& File : Files)
 	//	{
+	//		File.Open(EIOOpenMode::Read, EIODataType::Binary);
+
+	//		char Arr[100];
+	//		File.Read(Arr, 100);
+
 	//		UEngineSound::Load(File.GetFullPath());
 	//	}
-
-	//	UEngineSound::SoundPlay("anipang_ingame_wav.wav");
+	//	 UEngineSound::SoundPlay("anipang_ingame_wav.wav");
 	//}
 
-	int a = 0;
+	GEngine->CreateLevel<APlayGameMode>("PlayLevel");
+	GEngine->ChangeLevel("PlayLevel");
+
+
 }
